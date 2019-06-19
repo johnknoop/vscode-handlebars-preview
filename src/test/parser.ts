7478e1dcf8fc36b510f4fd3c426dd28a084e4e3f
John Knoop <knoopjohn@gmail.com>
2019-06-19 23:26:38 +0200

    WIP

--- src/test/parser.ts
+++ src/test/parser.ts
@@ -5,20 +5,20 @@ let hbs = `{{#>Partials/layout}}
   {{title}}
   {{author.name}}
 
-  {{#each reviewer}}
+  {{#each reviewer}}				1
 	  {{this.firstName}}
 	  {{./lastName}}
 	  {{email}}
 
-	  {{#each deepArray}}
+	  {{#each deepArray}}			2
 
-
+			{{ banana }}
 	  {{/each}}
   {{/each}}
 
   {{signature}}
   
-  {{#each otherthing}}
+  {{#each otherthing}}				3
 	  {{this.firstName}}
   {{/each}}
 
@@ -26,31 +26,33 @@ let hbs = `{{#>Partials/layout}}
 
 const arrayStack = [];
 
-class EachScope {
-	childScopes: EachScope[] = [];
+class ArrayScope {
+	childScopes: ArrayScope[] = [];
 	body = '';
 
-	constructor() {
-		this.parse();
+	constructor(private readonly arrayName: string) {
+		this.extractChildScopes();
 	}
 
-	private parse() {
+	private extractChildScopes() {
 		let nextStop: RegExpExecArray | null;
 
-		while ((nextStop = /{{(#|\/)each/.exec(hbs)) !== null) {
-			if (nextStop[1] === '/') {
+		while ((nextStop = /{{(#|\/)each( \w+)?.*}}/.exec(hbs)) !== null) {
+			if (nextStop[1] === '#') {
+				// New scope
+				this.body = hbs.substring(0, nextStop.index);
+				hbs = hbs.slice(nextStop.index + 1);
+				this.childScopes.push(new ArrayScope(nextStop[2]));
+			} else {
 				// Close the scope
 				this.body = hbs.substring(0, nextStop.index);
 				hbs = hbs.slice(nextStop.index + 1);
-			} else {
-				hbs = hbs.slice(nextStop.index + 1);
-				this.childScopes.push(new EachScope());
 			}
 		}
 	}
 }
 
-const scopes = new EachScope();
+const scopes = new ArrayScope('');
 
 console.log(JSON.stringify(scopes, null, 2));
 
